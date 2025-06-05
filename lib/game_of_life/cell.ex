409cdb558639c520b984fc55503bf607e05a8a84
Romain BERTHON <>
2025-06-05 22:10:44 +0200

    refactor

--- lib/game_of_life/cell.ex
+++ lib/game_of_life/cell.ex
@@ -2,9 +2,7 @@ defmodule GameOfLife.Cell do
   defmodule Coordinates do
     defstruct x: 0, y: 0
 
-    def get_cell_name(%Coordinates{:x => x, :y => y}) do
-      :"#{x}-#{y}"
-    end
+    def get_cell_name(%Coordinates{:x => x, :y => y}), do: :"#{x}-#{y}"
 
     def get_surrounding(
       %Coordinates{:x => x, :y => y} = coor,
@@ -23,28 +21,19 @@ defmodule GameOfLife.Cell do
   defmodule State do
     defstruct coordinates: %Coordinates{}, generation: 0, state: :dead
 
-    def nextState(current, 3) do
-      %State{current | state: :alive}
-    end
-
-    def nextState(%State{state: :alive} = current, 2) do
-      current
-    end
-
-    def nextState(current, _) do
-      %State{current | state: :dead}
-    end
+    def nextState(current, 3), do: %State{current | state: :alive}
+    def nextState(%State{state: :alive} = current, 2), do: current
+    def nextState(current, _), do: %State{current | state: :dead}
   end
 
   def start_link(%Coordinates{} = coor, initialState) do
     Agent.start_link(fn -> %State{
       coordinates: coor,
-      generation: 0,
       state: initialState
      } end, name: Coordinates.get_cell_name(coor))
   end
 
   def get_state(%Coordinates{} = coor) do
-    Agent.get(Coordinates.get_cell_name(coor), fn dic -> Map.get(dic, :state) end)
+    Agent.get(Coordinates.get_cell_name(coor), &(Map.get(&1, :state)))
   end
 end
