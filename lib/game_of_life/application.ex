304cb39b2d2f7e70c769f2584d76ecb4040e31c5
Romain BERTHON <>
2025-06-15 11:31:38 +0200

    feat: build initial state from list of cell state

--- lib/game_of_life/application.ex
+++ lib/game_of_life/application.ex
@@ -8,44 +8,55 @@ defmodule GameOfLife.Application do
   @impl true
   def start(_type, _args) do
     blinker = [
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 0}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 1}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 2}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 0}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 1}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 2}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 0}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 1}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 2}, :dead},
-      ]
-
-    grenouille = [
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 0}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 1}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 2}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 0, y: 3}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 0}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 1}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 2}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 1, y: 3}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 0}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 1}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 2}, :alive},
-        {%GameOfLife.Cell.Coordinates{x: 2, y: 3}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 3, y: 0}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 3, y: 1}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 3, y: 2}, :dead},
-        {%GameOfLife.Cell.Coordinates{x: 3, y: 3}, :dead},
+      [:dead,  :dead,  :dead  ],
+      [:alive, :alive, :alive ],
+      [:dead,  :dead,  :dead  ],
     ]
+
+    toad = [
+      [:dead,  :dead,  :dead,  :dead ],
+      [:dead,  :alive, :alive, :alive],
+      [:alive, :alive, :alive, :dead ],
+      [:dead,  :dead,  :dead,  :dead ],
+    ]
+
+    glider = [
+      [:dead,  :alive,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :alive, :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:alive, :alive, :alive, :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+      [:dead,  :dead,  :dead,  :dead, :dead, :dead, :dead, :dead, :dead, :dead, :dead ],
+    ]
+
+    # initial_state = blinker
+    # initial_state = toad
+    initial_state = glider
 
     children = [
       # Starts a worker by calling: GameOfLife.Worker.start_link(arg)
-      {GameOfLife.Game, grenouille}
+      {GameOfLife.Game, build_cell_coordinates initial_state}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GameOfLife.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp build_cell_coordinates(cells) do
+    cells
+    |> Enum.with_index(fn row, x ->
+      row
+      |> Enum.with_index(fn cell, y ->
+        {%GameOfLife.Cell.Coordinates{x: x, y: y}, cell}
+      end)
+    end)
+    |> Enum.flat_map(&(&1))
   end
 end
